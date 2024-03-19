f84b01ef484d72925b2afcaddc941b5561e7d39b
AndresAvenda <162192412+AndresAvenda@users.noreply.github.com>
2024-03-19 16:19:43 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+ function mostrarImagen() {
+    console.log("hola");
+    // Obtener la referencia al elemento de imagen
+    var imagen = document.getElementById("precionarRegalo");
+    // Mostrar la imagen cambiando su estilo
+    imagen.style.display = "block";
+    
+    
+ }
